e4bc985e737e368e8d457cf45688d8b31f0737d6
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-18 20:45:07 +0300

    feat[server]: add register and login methods to controller

--- server/src/main/java/com/group7/server/api/PlayerController.java
+++ server/src/main/java/com/group7/server/api/PlayerController.java
@@ -1,5 +1,6 @@
 package com.group7.server.api;
 
+import com.group7.server.model.ActivePlayer;
 import com.group7.server.model.Player;
 import com.group7.server.service.PlayerService;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("api/player")
+@RequestMapping("/api/player")
 @RestController
 public class PlayerController {
-    private final PlayerService playerService;
+    private final PlayerService mPlayerService;
 
     //TODO: Implement DTOs
-    @PostMapping("/add")
-    public ResponseEntity<Player> addStudent(@RequestBody Player player){
-        return ResponseEntity.ok().body(this.playerService.addPlayer(new Player()));
+    @PostMapping("/register")
+    public ResponseEntity<Player> register(@RequestBody Player player){
+        return ResponseEntity.ok().body(this.mPlayerService.register(new Player()));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<ActivePlayer> login(@RequestBody Player player){
+        return ResponseEntity.ok().body(this.mPlayerService.login(player));
+    }
+
 }
