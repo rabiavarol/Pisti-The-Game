10b7da72b11e2d09c3c41e8dfeb060cf70e41b22
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-18 20:44:35 +0300

    feat[server]: create active player model

---
+++ server/src/main/java/com/group7/server/model/ActivePlayer.java
@@ -0,0 +1,38 @@
+package com.group7.server.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "ACTIVE_PLAYER")
+public class ActivePlayer {
+    @Id
+    @GeneratedValue(generator = "ForeignGenerator")
+    @GenericGenerator(
+            name = "ForeignGenerator",
+            strategy = "foreign",
+            parameters = @Parameter(name = "property", value = "player"))
+    private Long id;
+
+    @Column(name = "LEVEL")
+    private int level;
+
+    @Column(name = "SCORE")
+    private int score;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn(name = "ID", referencedColumnName = "id")
+    private Player player;
+
+    public ActivePlayer(Player player){
+        this.player = player;
+    }
+}
