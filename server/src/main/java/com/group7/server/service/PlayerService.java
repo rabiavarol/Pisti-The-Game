33c6d1d0044e490375e7c9c7d20891f13045f8ae
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-18 20:45:35 +0300

    feat[server]: implement register and login methods to service

--- server/src/main/java/com/group7/server/service/PlayerService.java
+++ server/src/main/java/com/group7/server/service/PlayerService.java
@@ -1,7 +1,9 @@
 package com.group7.server.service;
 
+import com.group7.server.model.ActivePlayer;
 import com.group7.server.model.Player;
 
 public interface PlayerService {
-    Player addPlayer(Player player);
+    Player register(Player player);
+    ActivePlayer login(Player player);
 }
--- server/src/main/java/com/group7/server/service/PlayerServiceImpl.java
+++ server/src/main/java/com/group7/server/service/PlayerServiceImpl.java
@@ -1,18 +1,41 @@
 package com.group7.server.service;
 
+import com.group7.server.model.ActivePlayer;
 import com.group7.server.model.Player;
+import com.group7.server.repository.ActivePlayerRepository;
 import com.group7.server.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class PlayerServiceImpl implements PlayerService {
 
-    private final PlayerRepository playerRepository;
+    private final PlayerRepository mPlayerRepository;
+    private final ActivePlayerRepository mActivePlayerRepository;
 
     @Override
-    public Player addPlayer(Player player) {
-        return playerRepository.save(player);
+    public Player register(Player player) {
+        return mPlayerRepository.save(player);
+    }
+
+    @Override
+    public ActivePlayer login(Player player) {
+        ActivePlayer activePlayer = initializeActivePlayer(player);
+        if(activePlayer != null) {
+            return mActivePlayerRepository.save(activePlayer);
+        }
+        return null;
+    }
+
+    private ActivePlayer initializeActivePlayer(Player player) {
+        Optional<Player> repoPlayer = mPlayerRepository.findPlayerById(player.getId());
+        if(repoPlayer.isPresent()) {
+            return new ActivePlayer(repoPlayer.get());
+        }
+        return null;
     }
 }
