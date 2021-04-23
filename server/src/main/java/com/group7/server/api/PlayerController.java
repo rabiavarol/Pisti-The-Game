41a331dc5fbd093fdafdb05efc69d19731f2bd9f
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-24 02:24:51 +0300

    feat[server]: add javadoc comments in player controller

--- server/src/main/java/com/group7/server/api/PlayerController.java
+++ server/src/main/java/com/group7/server/api/PlayerController.java
@@ -7,6 +7,11 @@ import com.group7.server.service.PlayerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Responsible for authentication requests of the players.
+ * Deals with the register, login and logout requests.
+ *
+ */
 @RequiredArgsConstructor
 @RequestMapping("/api/player")
 @RestController
@@ -14,10 +19,25 @@ public class PlayerController {
 
     private final PlayerService mPlayerService;
 
-    //TODO: Implement DTOs
+    /**
+     * Handles player's register request. Utilizes playerService's method to deal with the request.
+     *
+     * @param authRequest the request which includes the necessary credentials of the player to register to the system;
+     *                    username, password and email cannot be null.
+     * @return the authentication response according to the success of the operation.
+     *                    If operation is successful; returns success status code;
+     *                                              ; error message is null.
+     *                    If operation is not successful; returns fail status code and the error message.
+     *
+     */
     @PostMapping("/register")
     public AuthResponse register(@RequestBody AuthRequest authRequest){
-        StatusCode statusCode = mPlayerService.register(new Player(authRequest.getUsername(), authRequest.getPassword(), authRequest.getEmail()));
+        StatusCode statusCode = mPlayerService.register(
+                new Player(
+                        authRequest.getUsername(),
+                        authRequest.getPassword(),
+                        authRequest.getEmail())
+        );
 
         if (statusCode.equals(StatusCode.SUCCESS)) {
             return new AuthResponse(StatusCode.SUCCESS, null);
@@ -25,19 +45,45 @@ public class PlayerController {
         return new AuthResponse(StatusCode.FAIL, "Register attempt failed!");
     }
 
+    /**
+     * Handles player's login request. Utilizes playerService's method to deal with the request.
+     *
+     * @param authRequest the request which includes the necessary credentials of the player to login to the system;
+     *                    username, password and email cannot be null.
+     * @return the authentication response according to the success of the operation.
+     *                    If operation is successful; returns success status code, JWT token and session id of the user
+     *                                              ; error message is null.
+     *                    If operation is not successful; returns fail status code and the error message.
+     */
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest authRequest){
         Object[] credentials = new Object[2];
-        StatusCode statusCode = mPlayerService.login(new Player(authRequest.getUsername(), authRequest.getPassword(), authRequest.getEmail()), credentials);
+        StatusCode statusCode = mPlayerService.login(
+                new Player(
+                        authRequest.getUsername(),
+                        authRequest.getPassword(),
+                        authRequest.getEmail()),
+                credentials
+        );
 
         if(statusCode.equals(StatusCode.SUCCESS)) {
             String token = (String) credentials[0];
             Long sessionId = (Long) credentials[1];
+
             return new LoginResponse(StatusCode.SUCCESS, null, token, sessionId);
         }
         return new AuthResponse(StatusCode.FAIL, "Login attempt failed!");
     }
 
+    /**
+     * Handles player's logout request. Utilizes playerService's method to deal with the request.
+     *
+     * @param deleteRequest the request which includes the session id of the player who sends the request.
+     * @return the authentication response according to the success of the operation.
+     *                    If operation is successful; returns success status code;
+     *                                              ; error message is null.
+     *                    If operation is not successful; returns fail status code and the error message.
+     */
     @DeleteMapping("/logout")
     public AuthResponse logout(@RequestBody DeleteRequest deleteRequest){
         StatusCode statusCode = mPlayerService.logout(deleteRequest.getSessionId());
