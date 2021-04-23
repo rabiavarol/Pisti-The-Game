c05b45b60250f4e4a56dfdd8bfe81556640e7e8d
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-24 00:51:41 +0300

    fix[server]: change fields of the player

--- server/src/main/java/com/group7/server/model/Player.java
+++ server/src/main/java/com/group7/server/model/Player.java
@@ -4,8 +4,12 @@ import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
@@ -13,13 +17,44 @@ import javax.persistence.*;
 @SequenceGenerator(name = "idgen", sequenceName = "PLAYER_SEQ")
 @Entity
 @Table(name="PLAYER")
-public class Player extends BaseModel {
+public class Player extends BaseModel
+        implements UserDetails, CredentialsContainer {
     @Column(name = "USERNAME", unique = true)
     private String username;
 
-    @Column(name = "PASSWORD", unique = true)
+    @Column(name = "PASSWORD")
     private String password;
 
     @Column(name = "EMAIL", unique = true)
     private String email;
+
+    @Override
+    public void eraseCredentials() {
+        password = null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- server/src/main/java/com/group7/server/repository/PlayerRepository.java
+++ server/src/main/java/com/group7/server/repository/PlayerRepository.java
@@ -4,10 +4,17 @@ import com.group7.server.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface PlayerRepository
         extends JpaRepository<Player, Long> {
-    Optional<Player> findPlayerById(long id);
+    Optional<Player> findById(long id);
+    Optional<Player> findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
