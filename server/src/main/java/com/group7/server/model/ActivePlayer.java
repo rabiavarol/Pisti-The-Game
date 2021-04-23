2ba26a8eb3bfaa6d1a26b3b4337f65ce47ab7582
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-24 00:51:25 +0300

    fix[server]: change fields of the active player

--- server/src/main/java/com/group7/server/model/ActivePlayer.java
+++ server/src/main/java/com/group7/server/model/ActivePlayer.java
@@ -1,40 +1,28 @@
 package com.group7.server.model;
 
-import com.sun.istack.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.sql.Delete;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "ACTIVE_PLAYER")
-public class ActivePlayer {
-    @Id
-    @GeneratedValue(generator = "ForeignGenerator")
-    @GenericGenerator(
-            name = "ForeignGenerator",
-            strategy = "foreign",
-            parameters = @Parameter(name = "property", value = "player"))
-    private Long id;
+public class ActivePlayer extends BaseModel {
+
+    @OneToOne
+    @JoinColumn(name = "PLAYER_ID", referencedColumnName = "id", unique = true)
+    private Player player;
 
     @Column(name = "LEVEL")
     private int level;
 
     @Column(name = "SCORE")
     private int score;
-
-    @OneToOne
-    @PrimaryKeyJoinColumn(name = "ID", referencedColumnName = "id")
-    private Player player;
-
-    @ManyToOne
-    @JoinColumn(name = "game_id")
-    private Game game;
 
     public ActivePlayer(Player player){
         this.player = player;
--- server/src/main/java/com/group7/server/repository/ActivePlayerRepository.java
+++ server/src/main/java/com/group7/server/repository/ActivePlayerRepository.java
@@ -1,7 +1,14 @@
 package com.group7.server.repository;
 
 import com.group7.server.model.ActivePlayer;
+import com.group7.server.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 public interface ActivePlayerRepository
-        extends JpaRepository<ActivePlayer, Long> {}
+        extends JpaRepository<ActivePlayer, Long> {
+    @Transactional
+    void deleteById(long id);
+}
