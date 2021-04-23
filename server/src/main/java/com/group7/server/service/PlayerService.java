4cb8b918683b714444274a4dcf13e3be0dd15e60
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-24 00:57:35 +0300

    refactor[server]: change method implementations of the player service

--- server/src/main/java/com/group7/server/service/PlayerService.java
+++ server/src/main/java/com/group7/server/service/PlayerService.java
@@ -1,9 +1,12 @@
 package com.group7.server.service;
 
+import com.group7.server.definitions.StatusCode;
 import com.group7.server.model.ActivePlayer;
 import com.group7.server.model.Player;
+import org.springframework.data.util.Pair;
 
 public interface PlayerService {
-    Player register(Player player);
-    ActivePlayer login(Player player);
+    StatusCode register(Player player);
+    StatusCode login(Player player, Object[] credentials);
+    StatusCode logout(Long id);
 }
--- server/src/main/java/com/group7/server/service/PlayerServiceImpl.java
+++ server/src/main/java/com/group7/server/service/PlayerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.group7.server.service;
 
+import com.group7.server.definitions.StatusCode;
 import com.group7.server.model.ActivePlayer;
 import com.group7.server.model.Player;
 import com.group7.server.repository.ActivePlayerRepository;
@@ -7,34 +8,64 @@ import com.group7.server.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class PlayerServiceImpl implements PlayerService {
 
+    private final AuthenticationService mAuthenticationService;
     private final PlayerRepository mPlayerRepository;
     private final ActivePlayerRepository mActivePlayerRepository;
 
     @Override
-    public Player register(Player player) {
-        return mPlayerRepository.save(player);
+    public StatusCode register(Player player) {
+        try {
+            mAuthenticationService.register(player);
+            return StatusCode.SUCCESS;
+        }
+        catch (Exception e) {
+            return StatusCode.FAIL;
+        }
     }
 
     @Override
-    public ActivePlayer login(Player player) {
-        ActivePlayer activePlayer = initializeActivePlayer(player);
-        if(activePlayer != null) {
-            return mActivePlayerRepository.save(activePlayer);
+    public StatusCode login(Player player, Object[] credentials) {
+        try {
+            String token;
+            Long sessionId;
+            if((token = mAuthenticationService.authenticate(player)) != null && (sessionId = initializeActivePlayer(player)) != null) {
+                credentials[0] = token;
+                credentials[1] = sessionId;
+                return StatusCode.SUCCESS;
+            }
+            return StatusCode.FAIL;
         }
-        return null;
+        catch (Exception e) {
+            return StatusCode.FAIL;
+        }
     }
 
-    private ActivePlayer initializeActivePlayer(Player player) {
-        Optional<Player> repoPlayer = mPlayerRepository.findPlayerById(player.getId());
-        if(repoPlayer.isPresent()) {
-            return new ActivePlayer(repoPlayer.get());
+    @Override
+    public StatusCode logout(Long id) {
+        try {
+            Optional<ActivePlayer> dbActivePlayer = mActivePlayerRepository.findById(id);
+            System.out.println(dbActivePlayer.get());
+            mActivePlayerRepository.deleteById(id);
+            return StatusCode.SUCCESS;
+        }
+        catch (Exception e) {
+            return StatusCode.FAIL;
+        }
+
+    }
+
+    private Long initializeActivePlayer(Player player) {
+        Optional<Player> dbPlayer = mPlayerRepository.findByUsername(player.getUsername());
+        if (dbPlayer.isPresent()) {
+            System.out.println(dbPlayer.get());
+            ActivePlayer activePlayer = mActivePlayerRepository.save(new ActivePlayer(dbPlayer.get()));
+            return activePlayer.getId();
         }
         return null;
     }
