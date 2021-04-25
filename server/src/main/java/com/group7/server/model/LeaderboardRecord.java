caf5394965946e7fdcc22ed0d431f53062f8a40d
rabia.varol <rabia.varol@metu.edu.tr>
2021-04-25 23:34:19 +0300

    feat[server]: implement the leaderboard record class

---
+++ server/src/main/java/com/group7/server/model/LeaderboardRecord.java
@@ -0,0 +1,35 @@
+package com.group7.server.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@SequenceGenerator(name = "idgen", sequenceName = "LEADERBOARD_RECORD_SEQ")
+@Entity
+@Table(name="LEADERBOARD_RECORD")
+/** Model of the leaderboard record created upon finish of a game*/
+public class LeaderboardRecord extends BaseModel {
+    /** Reference to the player who played the game */
+    @OneToOne
+    @JoinColumn(name = "PLAYER_ID", referencedColumnName = "id", unique = true)
+    private Player player;
+
+    /** Date when the game is finished */
+    @Column(name = "END_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date endDate;
+
+    /** Overall score */
+    @Column(name = "SCORE")
+    private int score;
+
+    public LeaderboardRecord(Player player, java.util.Date endDate, int score) {
+        this.player = player;
+        this.endDate = endDate;
+        this.score = score;
+    }
+}
--- server/src/main/java/com/group7/server/repository/LeaderboardRecordRepository.java
+++ server/src/main/java/com/group7/server/repository/LeaderboardRecordRepository.java
@@ -1,9 +1,9 @@
 package com.group7.server.repository;
 
-import com.group7.server.model.Leaderboard;
+import com.group7.server.model.LeaderboardRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeaderboardRepository
-        extends JpaRepository<Leaderboard, Long> {}
+public interface LeaderboardRecordRepository
+        extends JpaRepository<LeaderboardRecord, Long> {}
