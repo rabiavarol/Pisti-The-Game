9993f76c393a99fb53edd409896dc5116bc2e1fb
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-28 21:38:46 +0300

    feat[server]: create game environment

---
+++ server/src/main/java/com/group7/server/definitions/GameEnvironment.java
@@ -0,0 +1,38 @@
+package com.group7.server.definitions;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GameEnvironment {
+    private List<Short> mHandCards;
+    private Short       mNoHandCards;
+    private List<Short> mMiddleCards;
+    private List<Short> mScores;
+    private Boolean     mIsPisti;
+
+    public static GameEnvironment buildPlayerEnvironment(List<Short> handCards, List<Short> middleCards, List<Short> scores, Boolean isPisti) {
+        return new GameEnvironment(handCards, middleCards, scores, isPisti);
+    }
+
+    public static GameEnvironment buildPcEnvironment(Short noHandCards, List<Short> middleCards, List<Short> scores, Boolean isPisti) {
+        return new GameEnvironment(noHandCards, middleCards, scores, isPisti);
+    }
+
+    private GameEnvironment(List<Short> handCards, List<Short> middleCards, List<Short> scores, Boolean isPisti){
+        this.mHandCards = handCards;
+        this.mMiddleCards = middleCards;
+        this.mScores = scores;
+        this.mIsPisti = isPisti;
+    }
+
+    private GameEnvironment(Short noHandCards, List<Short> middleCards, List<Short> scores, Boolean isPisti){
+        this.mNoHandCards = noHandCards;
+        this.mMiddleCards = middleCards;
+        this.mScores = scores;
+        this.mIsPisti = isPisti;
+    }
+
+
+}
