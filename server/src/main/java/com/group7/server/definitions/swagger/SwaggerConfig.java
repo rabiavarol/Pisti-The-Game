e00af89512e0eaec28a14134fe7106f43353c1f0
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-04-30 16:46:54 +0300

    feat[server]: add swagger

--- server/src/main/java/com/group7/server/definitions/swagger/SwaggerConfig.java
+++ server/src/main/java/com/group7/server/definitions/swagger/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.group7.server.definitions.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +14,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+        return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.group7.server"))
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Pişti the Game")
+                .description("Game Designed by Group 7")
+                .contact(new Contact("Rabia Varol and Doruk Gerçel", "dodo.com", "rabia.varol@metu.edu.tr, doruk.gercel@metu.edu.tr"))
+                .license("Apache 10.05")
+                .version("1.0.0")
                 .build();
     }
 
