55ec677ebb889ec312591b62b26d58f146872d6f
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-05-12 13:16:22 +0300

    feat[client]: init client project

---
+++ client/src/main/java/com/group7/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.group7.client;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		Application.launch(UiApplication.class, args);
+	}
+
+}
