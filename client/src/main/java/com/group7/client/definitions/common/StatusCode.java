7a14ad08840c2e7fa75ad90ae5bf9f8b8dc66777
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-05-12 13:19:20 +0300

    feat[client]: create definitions for status and game

---
+++ client/src/main/java/com/group7/client/definitions/common/StatusCode.java
@@ -0,0 +1,7 @@
+package com.group7.client.definitions.common;
+
+/** Used in services and controllers to indicate operation success.*/
+public enum StatusCode {
+    SUCCESS,
+    FAIL
+}
---
+++ client/src/main/java/com/group7/client/definitions/game/GameEnvironment.java
@@ -0,0 +1,30 @@
+package com.group7.client.definitions.game;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GameEnvironment {
+    private List<Short> mHandCards;
+    private Short       mNoHandCards;
+    private List<Short> mMiddleCards;
+    private List<Short> mScores;
+    private Boolean     mIsPisti;
+
+    public GameEnvironment(List<Short> handCards, List<Short> middleCards, List<Short> scores, Boolean isPisti){
+        this.mHandCards = handCards;
+        this.mMiddleCards = middleCards;
+        this.mScores = scores;
+        this.mIsPisti = isPisti;
+    }
+
+    public GameEnvironment(Short noHandCards, List<Short> middleCards, List<Short> scores, Boolean isPisti){
+        this.mNoHandCards = noHandCards;
+        this.mMiddleCards = middleCards;
+        this.mScores = scores;
+        this.mIsPisti = isPisti;
+    }
+
+
+}
