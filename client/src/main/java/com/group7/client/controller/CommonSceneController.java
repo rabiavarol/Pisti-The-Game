910c556de2ca2a4512762738464b4b47e8b51671
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-05-12 13:21:29 +0300

    feat[client]: create common scene

---
+++ client/src/main/java/com/group7/client/controller/CommonSceneController.java
@@ -0,0 +1,13 @@
+package com.group7.client.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CommonSceneController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
