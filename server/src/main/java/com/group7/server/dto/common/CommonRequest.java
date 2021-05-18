c4640adcd384b32b257cda69e49b73a095109f74
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-05-18 12:25:11 +0300

    feat[server]: add descriptions and comments

--- server/src/main/java/com/group7/server/dto/common/CommonRequest.java
+++ server/src/main/java/com/group7/server/dto/common/CommonRequest.java
@@ -3,6 +3,7 @@ package com.group7.server.dto.common;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/** DTO used as a super class for all requests*/
 @Data
 @NoArgsConstructor
 public class CommonRequest {}
