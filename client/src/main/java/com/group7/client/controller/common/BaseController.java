cc8b0fcb8fbbae510ebf8180e2fc4d40564cd3dd
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-05-24 01:11:18 +0300

    feat[client]: implement new common methods to base controllers

--- client/src/main/java/com/group7/client/controller/common/BaseController.java
+++ client/src/main/java/com/group7/client/controller/common/BaseController.java
@@ -1,19 +1,36 @@
 package com.group7.client.controller.common;
 
+import com.group7.client.definitions.player.PlayerManager;
+import com.group7.client.definitions.screen.ScreenManager;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 /** Abstract controller which implements common controller methods*/
-public abstract class BaseController {
+public abstract class BaseController implements Initializable {
+    /** Reference to common screen manager*/
+    protected ScreenManager mScreenManager;
+    /** Holds active player information*/
+    protected PlayerManager mPlayerManager;
 
     /** Label displayed on screen*/
     @FXML protected Label informationLabel;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
+    /** Open error dialog*/
+    protected void displayError(String headerText, String message) {
+        displayAlert(Alert.AlertType.ERROR, "Error", headerText, message);
+    }
+
     /** Open alert dialog*/
-    protected void displayAlert(Alert.AlertType alertType, String title, String headerText, String message) {
+    private void displayAlert(Alert.AlertType alertType, String title, String headerText, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
@@ -22,7 +39,7 @@ public abstract class BaseController {
         loseFocus();
     }
 
-    protected void loseFocus() {
+    private void loseFocus() {
         informationLabel.requestFocus();
     }
 }
--- client/src/main/java/com/group7/client/controller/common/BaseNetworkController.java
+++ client/src/main/java/com/group7/client/controller/common/BaseNetworkController.java
@@ -1,26 +1,42 @@
 package com.group7.client.controller.common;
 
 import com.group7.client.definitions.common.StatusCode;
+import com.group7.client.definitions.network.NetworkManager;
+import com.group7.client.dto.authentication.LoginResponse;
 import com.group7.client.dto.common.CommonResponse;
-import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Objects;
 
 /** Abstract network controller which implements common network controller methods*/
-public abstract class BaseNetworkController extends BaseController
-        implements Initializable {
+public abstract class BaseNetworkController extends BaseController {
+    /** Reference to common network manager*/
+    protected NetworkManager mNetworkManager;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    protected boolean isOperationSuccess(CommonResponse commonResponse, StatusCode networkStatusCode, Class<? extends CommonResponse> responseType, String headerText) {
+        // Check if network operation is successful
+        if(isNetworkOperationSuccess(commonResponse, networkStatusCode, responseType)) {
+            // Check if operation is successful
+            if (isRequestedOperationSuccess(commonResponse)) {
+                return true;
+            } else {
+                displayError(headerText,
+                        Objects.requireNonNull(commonResponse).getErrorMessage());
+                return false;
+            }
+        } else {
+            displayError(headerText,
+                    "Network connection error occurred!");
+            return false;
+        }
+    }
 
     /** Checks whether the http response is present*/
-    protected boolean isNetworkOperationSuccess(CommonResponse commonResponse, StatusCode networkStatusCode) {
-        return commonResponse != null && networkStatusCode.equals(StatusCode.SUCCESS);
+    private boolean isNetworkOperationSuccess(CommonResponse commonResponse, StatusCode networkStatusCode, Class<? extends CommonResponse> responseType) {
+        return responseType.isInstance(commonResponse) && networkStatusCode.equals(StatusCode.SUCCESS);
     }
 
     /** Checks whether the response status code is success*/
-    protected boolean isOperationSuccess(CommonResponse commonResponse) {
+    private boolean isRequestedOperationSuccess(CommonResponse commonResponse) {
         return commonResponse != null && commonResponse.getStatusCode().equals(StatusCode.SUCCESS);
     }
 }
